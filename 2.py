11328ea21b8e072f70e47f3aa232e5c0f80b3e7d
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-04-18 15:52:59 -0600

    Create 2.py
    Strings and the Print Function in Python

---
+++ 2.py
@@ -0,0 +1,13 @@
+# Challenge
+# Modify the name on Dot’s passport so it can be recognized by passport control. Which combination of string functions will give us the right results?
+
+# name: Dot
+
+# Required format: #DOT!
+
+name = '  Dot  '
+
+# SOLUTION
+'#' + name.strip().upper() + '!'
+
+# '#DOT!'
