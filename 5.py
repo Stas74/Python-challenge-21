f4fa3ed1f0c5e5b200d464567cf964c411f280b5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-04-20 14:36:04 -0600

    Create 5.py
    Control Structures: for loops and if statements

---
+++ 5.py
@@ -0,0 +1,25 @@
+# Control Structures: for loops and if statements
+
+# Python "for" Loops (Definite Iteration):  https://realpython.com/python-for-loop/
+# Conditional Statements in Python:  https://realpython.com/python-conditional-statements/
+
+# Challenge
+# Dot wants to visit all the London landmarks where the wait time is less than 15 minutes. We have a dictionary called landmarks, within which landmark names are keys and wait times are their respective values.
+
+# Using Python, develop a list of the landmarks where the wait time is less than 15 minutes. What will be the length of the list?
+
+# dictionary where 
+landmarks = {
+    "Big Ben": 12,
+    "Tower Bridge": 25,
+    "Buckingham Palace": 15,
+    "Madame Tussauds": 25,
+    "London Eye": 40,
+    "Tower of London": 25,
+    "Emirates Air Line cable car": 16,
+    "London Transport Museum": 7,
+    "Wembley Stadium": 8,
+    "Hyde Park": 0,
+    "The View from The Shard": 14
+}
+
