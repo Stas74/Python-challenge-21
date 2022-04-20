b953ca783cd06c7ff8c063b3f976bf8129d49fc3
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-04-20 14:04:26 -0600

    Create 4.py
    Dictionaries in Python

---
+++ 4.py
@@ -0,0 +1,40 @@
+# Challenge
+# Dot doesn't travel to Europe often, so they decide to take the most expensive option for each course as well as drink. Create a new dictionary called meals that will contain the names of the courses as the keys (starters, mains...), and the name of the food or drink item as the values.
+
+# After assembling the dictionary appropriately, when Dot gives a 10% tip on this meal, how much will the tip come out to?
+
+starters = {
+    "Potato Pancakes": 7.99,
+    "Salami Platter": 10.29,
+    "Brezel": 6.99,
+    "Maultaschen": 9.99,
+    "Fried Potatoes": 4.99
+}
+
+mains = {
+    "Braised Beef Short Ribs": 18.99,
+    "Paprika Beef Goulash": 15.5,
+    "Jager Schnitzel": 16.99,
+    "House-mase Bratwurst": 11.99,
+    "Kasespatzle": 14.99,
+    "German Ravioli": 12.79,
+    "Curry Wurst": 10.99
+}
+
+desserts = {
+    "Chilled Chocolate Fondant": 7.9,
+    "Pepermint Crisp Tart": 5.9,
+    "Ginger Cobbler": 6.9,
+    
+}
+
+beers = {
+    "Stigel Radler": 6.9,
+    "Munich Lager": 7.9,
+    "Kong Ludwig Weissbier": 8.9,
+    "Warsteiner Punkel": 7.5,
+}
+
+
+# if you want to see the keys of the dictionary in the list: keys = list(starters.keys())
+# if you want to see the keys of the dictionary in the list: values = list(starters.values())
