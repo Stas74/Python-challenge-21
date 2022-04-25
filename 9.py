263711c7975b232fa1d18059506a55678c8fbf02
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-04-25 13:00:48 -0600

    Update 9.py

--- 9.py
+++ 9.py
@@ -78,7 +78,23 @@ print(len(filtered_df_alco))
 filtered_df_class = df[df['Class'] == 3]
 print(len(filtered_df_class))
 
+# 86
+# 48
+
+# SOLUTION
+
+print(len(df[df.Alcohol < 13]))
+print(len(df[df.Class == 3]))
+
 # Stretch Questions
 
 # How many wines have a level of magnesium between 90 and 100?
 # How many wines have a level of magnesium higher than 90, and a percentage of alcohol lower than 13.5%?
+
+# STRETCH SOLUTION
+
+print(len(df[df.Magnesium.between(90,100)]))
+print(len(df[(df.Magnesium > 90) & (df.Alcohol < 13.5)]))
+
+# 48
+# 71
